b985259ad26117c81890a4ad32247f0e56a079af
betterclient <iamanaltlolwhypog@gmail.com>
2025-07-04 01:42:11 +0300

    Hello world

---
+++ device/samsung/a9lte/README.md
@@ -0,0 +1,13 @@
+# Universal device tree of Samsung Galaxy A9 2018 (SM-A920F) for building custom recovery
+
+## twrp-9.0
+Manifest: https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni/tree/twrp-9.0
+
+## fox_9.0
+Manifest: https://gitlab.com/OrangeFox/Manifest/-/tree/fox_9.0
+
+## shrp-9.0
+Manifest: https://github.com/SHRP/platform_manifest_twrp_omni/tree/v3_9.0
+
+## How to compile
+`cd <source-dir>; . build/envsetup.sh; export ALLOW_MISSING_DEPENDENCIES=true; export LC_ALL=C; mka recoveryimage`
---
+++ device/samsung/a9lte/a9lte.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit LineageOS base configurations
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit your device-specific configuration
+$(call inherit-product, device/samsung/a9lte/device.mk)
+
+# Device identity
+PRODUCT_DEVICE := a9lte
+PRODUCT_NAME := lineage_a9lte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A920F
+PRODUCT_MANUFACTURER := samsung
+
+# Assert device
+TARGET_OTA_ASSERT_DEVICE := a9lte
